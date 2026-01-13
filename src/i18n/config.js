4b9b56be88eefa8add5862f58dab46de53c6dcd1
Kiterion83 <giuseppe.pasquale@outlook.com>
2026-01-13 12:28:55 +0100

    Add files via upload

--- src/i18n/config.js
+++ src/i18n/config.js
@@ -1,1 +1,24 @@
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
 
+import it from './locales/it.json'
+import en from './locales/en.json'
+
+// Get saved language or default to IT
+const savedLanguage = localStorage.getItem('pts-language') || 'it'
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources: {
+      it: { translation: it },
+      en: { translation: en },
+    },
+    lng: savedLanguage,
+    fallbackLng: 'it',
+    interpolation: {
+      escapeValue: false,
+    },
+  })
+
+export default i18n
