def2ce28076521a557caac94f08c0f2c195f9644
Kiterion83 <giuseppe.pasquale@outlook.com>
2026-01-14 15:45:57 +0100

    Add files via upload

--- src/pages/WorkPackages.jsx
+++ src/pages/WorkPackages.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useMemo } from 'react';
-import { supabase } from '../supabaseClient';
+import { supabase } from '../lib/supabaseClient';
 import WorkPackagesGantt from '../components/WorkPackagesGantt';
 
 // ============================================================================
